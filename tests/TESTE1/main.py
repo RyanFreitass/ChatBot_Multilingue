f8feabdcb4a9c3e0d3f56648ac2801418687fb9c
Ryan Freitas <ryanvitorfreitas@gmail.com>
2025-06-10 20:08:22 -0300

    Changed

--- tests/TESTE1/main.py
+++ tests/TESTE1/main.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import ttk, messagebox
 from googletrans import Translator
 from mensagens_teste.perguntas import PerguntasUsuario
+from Expressoes_regulares.validar import Validacao
 
 class ConsuladoApp:
     def __init__(self, root):
